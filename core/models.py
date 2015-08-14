3db434d03e9402c886a449ea06a05c51072960d3
Bruno Geninatti <brunogeninatti@gmail.com>
2015-08-14 18:03:06 -0300

    datos de timer en modelo y comentarios

--- core/models.py
+++ core/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 
 class Hackaton(models.Model):
-    user = models.ForeignKey('auth.User',related_name='hackatones')
-    nombre = models.CharField(max_length=50)
-    background_img = models.URLField(default='media/default_background.png')
-    hora_inicio = models.DateTimeField()
-    hora_final = models.DateTimeField()
-    tw_hashtag = models.CharField(max_length=20)
-    nombre_hackdash = models.CharField(max_length=20)
+    user = models.ForeignKey('auth.User',related_name='hackatones')  # Nombre de usuario
+    nombre = models.CharField(max_length=50)  # Nombre de hackaton
+    background_img = models.URLField(default='media/default_background.png')  # Imagen a usar como background
+    hora_inicio = models.DateTimeField()  # Hora de inicio de hackaton. El datetime se saca con el request.
+    hora_final = models.DateTimeField()  # Hora en la que finaliza la hackaton
+    tw_hashtag = models.CharField(max_length=20)  # Hashtag de los twits que se muestran
+    tw_time = models.IntegerField()  # Tiempo en segundos antes de mostrar el siguiente twit
+    page_time = models.IntegerField()  # Tiempo en segundos antes de cambiar a la siguiente pagina (timer, twits, proyectos)
+    nombre_hackdash = models.CharField(max_length=20)  # Nombre del proyecto en HackDash
     created = models.DateTimeField(auto_now_add=True)
     udpated = models.DateTimeField(auto_now=True)
 
