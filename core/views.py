5df2fd35a0434a94fa255079a8efa6bae92fe050
Juan Abdala <abdalajc@gmail.com>
2015-08-14 17:56:16 -0300

    view and url with index

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+
+def index(request, *args, **kwargs):
+
+    return render_to_response("index.html", locals(),
+                              context_instance=RequestContext(request))
